235e98c3fdb1749e46f8e69efb63fb4147bdd652
ofion <ofion@bk.ru>
2021-08-07 12:24:21 +0300

    .

--- src/pages/MethodistsReport.vue
+++ src/pages/MethodistsReport.vue
@@ -3,9 +3,6 @@
 
     <v-container fluid>
       <v-row>
-        <v-col>
-          <PersonSelector @selected="changed"/>
-        </v-col>
         <v-col style="text-align: left">
 
           <v-menu
@@ -45,26 +42,30 @@
             </v-date-picker>
           </v-menu>
         </v-col>
+        <v-col>
+          <PersonSelector @selected="changed"/>
+        </v-col>
+
 
       </v-row>
       <v-row>
-        <v-col>
-          <v-btn color="primary" v-if="dates.length" @click="create"> Сформировать отчет</v-btn>
+        <v-col style="text-align: center">
+          <v-btn color="primary" @click="create"> Сформировать отчет</v-btn>
         </v-col>
       </v-row>
     </v-container>
 
-    <div class="flex column">
+    <div class="flex column" v-if="table.length">
 
       <div class="table-head grid">
 
         <span class="table-head__title flex">Ф.И.О. ответственного</span>
         <span class="table-head__title flex">Отчетная дата</span>
-        <span class="table-head__title flex">Наименован иезаказчика</span>
+        <span class="table-head__title flex">Наименование заказчика</span>
         <span class="table-head__title flex">Сделка и задача</span>
         <span class="table-head__title flex">Тип сделки</span>
         <span class="table-head__title flex">Время на проект, ч.</span>
-        <span class="table-head__title flex">Время за отчетную сдачу, ч.</span>
+        <span class="table-head__title flex">Время за отчетную дату, ч.</span>
 
       </div>
 
@@ -72,31 +73,71 @@
     <table class="table-line">
 
       <tr v-for="(row, indexPerson) in table" :key="indexPerson" class="table-line__row">
-        <td class="table-line__name">{{ row.name }}</td>
+        <td class="table-line__name"><a target="_blank"
+                                        :href="'https://coko1.bitrix24.ru/company/personal/user/'+row.userId+'/'">{{
+            row.name
+          }}</a></td>
         <td class="main-table__wrap">
           <table class="main-table">
 
-            <tr class="main-table__date-wrap" :key="indexDate" v-for="(dateElem, indexDate) in row.dates">
+            <tr class="main-table__date-wrap" :key="indexDate" v-for="(dateElem, indexDate) in row.dates.elements">
               <td class="main-table__date">{{ dateElem.date }}</td>
               <td>
                 <table>
-                  <tr v-for="(elem, indexElem) in dateElem.elements" :key="indexElem">
-                    <td class="main-table__company">{{ elem.company }}</td>
-                    <td class="main-table__task">{{ elem.task }}</td>
-                    <td class="main-table__type">{{ elem.type }}</td>
-                    <td class="main-table__time">{{ elem.time }}</td>
+                  <tr v-for="(elem, indexElem) in dateElem.rows" :key="indexElem">
+                    <td class="main-table__company"><a
+                        target="_blank"
+                        :href="'https://coko1.bitrix24.ru/crm/company/details/'+elem.companyId+'/'">{{
+                        elem.company
+                      }}</a>
+                    </td>
+                    <td class="main-table__task"><a
+                        target="_blank"
+                        :href="'https://coko1.bitrix24.ru/company/personal/user/515/tasks/task/view/'+elem.taskId+'/'">Задача
+                      №{{ elem.taskId }}</a>
+                      <br/>
+                      <a
+                          target="_blank"
+                          :href="'https://coko1.bitrix24.ru/crm/deal/details/'+elem.dealId+'/'">Сделка
+                        №{{ +elem.dealId }}</a>
+                    </td>
+                    <td class="main-table__type"><span v-for="(type, indexType) in elem.types"
+                                                       :key="indexType">{{
+                        (indexType !== 0 ? ", " : ' ') + type
+                      }}</span></td>
+                    <td class="main-table__time">
+                      <span v-if="elem.planTime">
+                        План: {{ elem.planTime }}
+                    </span>
+                      <span data-tooltip="Укажите план часы в сделке - в поле 'Время на проект, ч' (UF_CRM_1628254497)"
+                            v-else>
+                        План не задан.
+                      </span>
+                      <br/>
+                      <span v-bind:class="[Math.floor(elem.time/60/60 * 100) / 100>elem.planTime  ? 'errorClass' : '']">Факт: {{
+                          Math.floor(elem.time / 60 / 60 * 100) / 100
+                        }} ч. </span>
+                    </td>
                   </tr>
                 </table>
+              <td class="main-table__date-result">
+                <span>План: {{ dateElem.summaryPlanTime }}</span>
+                <br/>
+                <span>Факт: {{ Math.floor(dateElem.summaryTime / 60 / 60 * 100) / 100 }}</span>
+
               </td>
-              <td class="main-table__date-result">{{ dateElem.date_result }}</td>
             </tr>
 
             <tr class="result">
               <td class="main-table__sum">Итого за период</td>
               <td colspan="6" class="main-table__result">
-                <div class="main-table__summary"><span v-for="(sum, indexSumm) in row.summary" :key="indexSumm">{{
-                    sum
-                  }}</span></div>
+                <div class="main-table__summary">{{row.dates.summary.summaryTime / 60 / 60}}
+                  <br/>
+                  {{  row.dates.summary.summaryPlanTime }}
+                  <br/>
+                  {{  row.dates.summary.taskCount }}
+                  <br/>
+                </div>
               </td>
             </tr>
           </table>
@@ -126,52 +167,7 @@ export default {
   data() {
     return {
       menu: false,
-      table: [
-        {
-          name: "Петров А.И.",
-          dates: [
-            {
-              date: '12.05.21',
-              date_result: '9',
-              elements: [
-                {
-                  company: 'ООО СЕВЕРСЕВЕРСЕВЕ РСЕВЕРСЕВЕР',
-                  task: '1123331',
-                  type: 'АТЭЛ, АТОТ, ВАТЭЛ, АТОТВ',
-                  time: '1.5'
-                },
-                {company: 'ООО СЕВЕР222', task: '1123', type: 'ADOF', time: '1.5'},
-                {company: 'ООО СЕВЕР333', task: '11231', type: 'ADOF', time: '1.5'},
-                {company: 'ООО СЕВЕР444', task: '131', type: 'ADOF', time: '1.5'}
-              ]
-            },
-
-            {
-              date: '16.05.21',
-              date_result: '4',
-              elements: [
-                {company: 'ООО СЕ234ВЕР', task: '11232331', type: 'ADOF', time: '1.5'},
-                {company: 'ООО СЕВЕР223422', task: '1123', type: 'ADOF', time: '1.5'},
-              ]
-            },
-            {
-              date: '18.05.21',
-              date_result: '4',
-              elements: [
-                {company: 'ООО СЕ234ВЕР', task: '11232331', type: 'ADOF', time: '1.5'},
-                {company: 'ООО СЕВЕР223422', task: '1123', type: 'ADOF', time: '1.5'},
-              ]
-            },
-
-          ],
-          summary: ["Количество поставленных задач: 8 (в работе : 1) ",
-            "Норма времени за период (план): 32 ч. ",
-            "Затраченное время (факт): 34 ч. ",
-            "Время отставания от плана: 2 ч. ( или  значение «нет») ",]
-        },
-
-
-      ],
+      table: [],
 
       dates: [],
       allUsers: [],
@@ -198,32 +194,32 @@ export default {
       this.$refs.menu.save(this.dates);
     },
 
-    async fetchTimeData() {
-      let allTimes;
+    async fetchTimeData(userId) {
+      let timeRecords;
       await this.callMethod(
           'task.elapseditem.getlist',
-          [{'ID': 'desc'}, {'>=CREATED_DATE': '2021-01-01', '<CREATED_DATE': '2021-09-01', "USER_ID": "493"}]
+          [{'ID': 'desc'}, {'>=CREATED_DATE': '2021-01-01', '<CREATED_DATE': '2021-09-01', "USER_ID": userId}]
       ).then(function (res) {
-        allTimes = res;
+        timeRecords = res;
       });
 
-      return allTimes;
+      return timeRecords;
     },
 
-    async fetchTasks(allTimes) {
+    async fetchTasks(timeRecords) {
       let needleTasks;
       await this.callMethod(
           'tasks.task.list',
           {
             filter: {
-              'ID': allTimes.map((el) => {
+              'ID': timeRecords.map((el) => {
                 return el.TASK_ID
               }),
               'TITLE': 'Производство документов_%',
             },
             select: [
               'ID',
-              'ID',
+              'STATUS',
               'UF_CRM_TASK'
             ]
           }).then(function (res) {
@@ -237,13 +233,14 @@ export default {
             }
           }
       )
+
       return needleTasks;
 
     },
 
-    filterDealIds(allTimes, needleTasks) {
+    filterDealIds(timeRecords, needleTasks) {
       let dealIDs = [];
-      allTimes.forEach((el) => {
+      timeRecords.forEach((el) => {
         let task = needleTasks.find((tsk) => tsk.id === el.TASK_ID);
         if (task && task.ufCrmTask) {
           task.dealId = task.ufCrmTask.find((crmId) => {
@@ -256,29 +253,83 @@ export default {
       return dealIDs
     },
 
-    sumSpentTime(allTimes, needleTasks, deals) {
+    sumSpentTime(timeRecords, needleTasks, deals) {
       let res = [];
-      allTimes.forEach((el) => {
-        let date = new Date(el.CREATED_DATE);
+      let summary = {
+        summaryTime: 0,
+        taskCount: 0,
+        openTaskCount: 0,
+        summaryPlanTime: 0,
+        summaryRecordedTasks: [],
+      };
+      timeRecords.forEach((timeRecord) => {
+        let date = new Date(timeRecord.CREATED_DATE);
         date = date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear();
-        let task = needleTasks.find((tsk) => tsk.id === el.TASK_ID);
+        let task = needleTasks.find((tsk) => tsk.id === timeRecord.TASK_ID);
         let deal;
-        if (task) deal = deals.find((dl) => dl.ID === task.dealId);
-        let curDateData = res.find((el) => el.date === date);
+        if (task) {
+          deal = deals.find((dl) => dl.ID === task.dealId);
+          let curDateData = res.find((dta) => dta.date === date);
+          if (!curDateData) {
+            let rows = [];
+            rows.push({
+              taskId: task.id,
+              time: parseInt(timeRecord.SECONDS),
+              companyId: deal.COMPANY_ID,
+              company: deal.companyName,
+              dealId: deal.ID,
+              types: deal.type,
+              planTime: deal.UF_CRM_1628254497,
+            });
 
-        if (!curDateData) {
-          let rows = [];
-          rows.push({task,deal});
-          res.push({date: date, rows: rows, spentTime: parseInt(el.SECONDS)});
-        } else {
-          if (!curDateData.rows.find((el) => el.id === task.id)) {
-            curDateData.rows.push({task,deal});
+
+            let recordedDeals = [];
+            recordedDeals.push(deal.ID)
+
+            res.push({
+              date: date,
+              rows: rows,
+              summaryTime: parseInt(timeRecord.SECONDS),
+              recordedDeals: recordedDeals,
+              summaryPlanTime: deal.UF_CRM_1628254497 ? parseInt(deal.UF_CRM_1628254497) : 0
+            });
+
+          } else {
+            let innerRow = curDateData.rows.find((ele) => ele.taskId === task.id)
+            if (!innerRow) {
+              curDateData.rows.push({
+                taskId: task.id,
+                time: parseInt(timeRecord.SECONDS),
+                companyId: deal.COMPANY_ID,
+                company: deal.companyName,
+                dealId: deal.ID,
+                types: deal.type,
+                planTime: deal.UF_CRM_1628254497,
+              });
+            } else {
+              innerRow.time += parseInt(timeRecord.SECONDS);
+            }
+            if (curDateData.recordedDeals.indexOf(deal.ID) === -1) {
+              curDateData.summaryPlanTime += deal.UF_CRM_1628254497 ? parseInt(deal.UF_CRM_1628254497) : 0;
+              curDateData.recordedDeals.push(deal.ID);
+            }
+            curDateData.summaryTime += parseInt(timeRecord.SECONDS);
           }
-          curDateData.spentTime += parseInt(el.SECONDS);
+
+
+          summary.summaryTime += parseInt(timeRecord.SECONDS);
+
+          if (summary.summaryRecordedTasks.indexOf(task.id) === -1) {
+            console.log(task)
+            if (task.status !== 5) summary.openTaskCount++;
+            summary.taskCount++;
+            summary.summaryPlanTime += deal.UF_CRM_1628254497 ? parseInt(deal.UF_CRM_1628254497) : 0;
+            summary.summaryRecordedTasks.push(task.id);
+          }
         }
       })
-      console.log(res)
-      return res;
+
+      return {elements: res, summary: summary};
 
     },
 
@@ -316,18 +367,23 @@ export default {
     },
 
     async create() {
+      this.table = [];
       let prop = await this.fetchPropType();
 
-      let timeData = await this.fetchTimeData();
-      let tasks = await this.fetchTasks(timeData);
-      let deals = await this.fetchDeals(this.filterDealIds(timeData, tasks), prop)
+      let users = this.selectedUsers.length ? this.selectedUsers : this.allUsers.map((usr) => usr.value);
+      for (let userId of users) {
 
-      this.sumSpentTime(timeData, tasks, deals);
+        let timeData = await this.fetchTimeData(userId);
+        let tasks = await this.fetchTasks(timeData);
+        let deals = await this.fetchDeals(this.filterDealIds(timeData, tasks), prop);
+        let data = this.sumSpentTime(timeData, tasks, deals);
+        if (data) {
+
+          this.table.push({name: this.getUserName(userId), userId: userId, dates: data});
+        }
+      }
 
 
-
-
-      // console.log(result)
 
 
     },
@@ -349,7 +405,7 @@ export default {
 }
 
 .table-head {
-  grid-template-columns: 156px 99px 333px 302px 98px 100px 100px;
+  grid-template-columns: 156px 117px 320px 158px 148px 167px 121px;
   align-items: center;
   gap: 2px;
 }
@@ -418,7 +474,7 @@ export default {
 }
 
 .main-table__date, .main-table__date-result {
-  width: 100px;
+  width: 120px;
 }
 
 .main-table__date-wrap:not(:last-child) {
@@ -440,7 +496,7 @@ export default {
 
 .main-table tr td table tr {
   display: grid;
-  grid-template-columns: 40% 1fr  100px  100px;
+  grid-template-columns: 40% 1fr  150px  170px;
 }
 
 .main-table__summary {
@@ -458,4 +514,31 @@ export default {
   text-align: left;
 }
 
+.errorClass {
+  color: red;
+}
+
+[data-tooltip] {
+  position: relative;
+}
+
+[data-tooltip]::after {
+  content: attr(data-tooltip);
+  position: absolute;
+  width: 300px;
+  left: 0;
+  top: 0;
+  background: #3989c9;
+  color: #fff;
+  padding: 0.5em;
+  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
+  pointer-events: none;
+  opacity: 0;
+  transition: 1s;
+}
+
+[data-tooltip]:hover::after {
+  opacity: 1;
+  top: 2em;
+}
 </style>
